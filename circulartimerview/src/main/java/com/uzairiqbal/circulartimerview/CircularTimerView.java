9a367f4ad4eb63e1c220ccfd2be2a18dd399243f
uzair91 <uzairiqbal91@gmail.com>
2019-10-24 13:54:04 +0500

    made class

---
+++ circulartimerview/src/main/java/com/uzairiqbal/circulartimerview/CircularTimerView.java
@@ -0,0 +1,25 @@
+package com.uzairiqbal.circulartimerview;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class CircularTimerView extends RelativeLayout {
+    public CircularTimerView(Context context) {
+        super(context);
+    }
+
+    public CircularTimerView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CircularTimerView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public CircularTimerView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+
+
+}
